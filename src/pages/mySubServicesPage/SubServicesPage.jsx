f1e44d69af4df82219702b4c748e7a93e365bdfa
Mohamed Amged <mohamedamged55555@gmail.com>
2024-10-09 20:09:20 +0300

    inialize srvices page and sub services page ui

--- src/pages/mySubServicesPage/SubServicesPage.jsx
+++ src/pages/mySubServicesPage/SubServicesPage.jsx
@@ -1,6 +1,6 @@
 import MyHomeSlider from "../../components/myHomeSliderSec/MyHomeSlider";
 import sliderImg1 from '../../assets/home-insights/8be20325d2160343393bf6f345b1b8fb.jpeg'
-import MySubServices from "../../components/mySubServicesSec/mySubServices";
+import MySubServices from "../../components/mySubServicesSec/MySubServices";
 export default function SubServicesPage() {
     return (
         <>
