576a5ad968931dcdc20a739b4728009632c9d001
Mohamed Amged <mohamedamged55555@gmail.com>
2024-10-17 04:25:06 +0300

    fixing bug error when refresh on any page except home in vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Make sure your output folder is correct; typically it's 'dist' by default
+    outDir: 'dist',
+  },
+  server: {
+    // Ensures Vite server uses HTML5 history mode for routing
+    historyApiFallback: true,
+  }
 })
